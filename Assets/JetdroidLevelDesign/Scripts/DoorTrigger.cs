aa0fca702ea9ab8fdbb7114b56c6a8b6715e8d2f
Peter Parnes <peter@parnes.com>
2017-04-05 17:11:02 +0200

    Added door with animations and trigger

---
+++ Assets/JetdroidLevelDesign/Scripts/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+	public Door door;
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.gameObject.tag == "Player") {
+			door.Open ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D target) {
+		if(target.gameObject.tag == "Player") {
+			door.Close ();
+		}
+	}
+}
